91aa62cc9093e4dcf7e44589829a2c5236b2764d
Maged Ibrahim <magedibra@yahoo.com>
2024-08-04 13:07:12 +0300

    Remove unwanted constant as middleware is not used for authentication

--- src/consts/routes.ts
+++ src/consts/routes.ts
@@ -1,5 +1,3 @@
 export const DEFAULT_LOGIN_REDIRECT = "/";
 
-export const authRoutes = ["/login", "/register"];
-
-export const DEFAULT_SIGNOUT_REDIRECT = authRoutes[0];
+export const DEFAULT_UNAUTHENTICATED_REDIRECT = "/login";
