cb424e15bb3ba62764e0263c4a5e36d46888a24f
Maged Ibrahim <magedibra@yahoo.com>
2024-08-12 05:13:56 +0300

    Force the user to update their profile every 1 minute

--- src/actions/users.ts
+++ src/actions/users.ts
@@ -16,6 +16,17 @@ export async function updateUserAction(
 ) {
   const session = await getServerAuthSession();
   if (!session) return { error: "must-be-logged-in" };
+  const user = await getUserById(session.user.id);
+  if (!user) return { error: "must-be-logged-in" };
+
+  if (user.updatedAt) {
+    // Force the user to update their profile every 1 minute
+    const timePassedSinceLastUpdateInSeconds =
+      (new Date().getTime() - new Date(user.updatedAt).getTime()) / 3600;
+    const isUserAbleToUpdate = timePassedSinceLastUpdateInSeconds > 60;
+    if (!isUserAbleToUpdate) return { error: "please-wait" };
+  }
+
   await db.user.update({
     where: {
       id: session.user.id,
@@ -25,4 +36,11 @@ export async function updateUserAction(
     },
   });
   return { message: "updated" };
+}
+
+export async function getUserById(id: string) {
+  const user = await db.user.findUnique({
+    where: { id },
+  });
+  return user;
 }
