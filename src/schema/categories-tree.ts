d309ff8aa613a0e69a609dacea253d1c40a27a23
Maqed <magedibra@yahoo.com>
2024-08-24 04:01:31 +0300

    Using real data instead of the mock data

--- src/schema/categories-tree.ts
+++ src/schema/categories-tree.ts
@@ -1,47 +1,34 @@
 export type CategoryTreeItem = {
-    name: string;
-    description: string | undefined;
-    categories: CategoryTreeItem[] | undefined;
-}
+  name: string;
+  description: string | undefined;
+  categories: CategoryTreeItem[] | undefined;
+};
 
 export const categoriesTree = {
-    categories: [
+  categories: [
+    {
+      name: "vehicles",
+      categories: [
         {
-            name: "Vehicles",
-            description: "Epic vehicles description.",
-            categories: [
-                {
-                    name: "Electric Cars",
-                    categories: [
-                        {
-                            name: "Tesla",
-                            description: "Tesla CARS BatChest!"
-                        }
-                    ]
-                },
-                {
-                    name: "Gas Cars",
-                    categories: [
-                        {
-                            name: "BMW",
-                            description: "BMW CARS BatChest!"
-                        },
-                        {
-                            name: "Lambourghini",
-                            description: "LAMBO POGGERS!"
-                        }
-                    ]
-                },
-            ]
+          name: "electric-cars",
+          categories: [
+            {
+              name: "Tesla",
+            },
+          ],
         },
         {
-            name: "Mobiles & Tablets",
-            description: "For the zoomers.",
-            categories: [
-                { name: "Mobile Phones" },
-                { name: "Tablets" },
-                { name: "Mobile & Tablet Accessories" },
-            ]
-        }
-    ]
+          name: "gas-cars",
+        },
+      ],
+    },
+    {
+      name: "mobiles-and-tablets",
+      categories: [
+        { name: "mobile-phones" },
+        { name: "tablets" },
+        { name: "accessories" },
+      ],
+    },
+  ],
 };
