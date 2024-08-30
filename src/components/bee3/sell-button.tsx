890daf20db9c63075c0520c98f59ead8cd6bf7d6
Maqed <magedibra@yahoo.com>
2024-08-30 22:03:37 +0300

    Fix bug

--- src/components/bee3/sell-button.tsx
+++ src/components/bee3/sell-button.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from "@/navigation";
 import { useTranslations } from "next-intl";
 import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
