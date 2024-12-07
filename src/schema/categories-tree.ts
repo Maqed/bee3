0d3362ce325c84fc91534f873b95f02e19536d30
Maqed <magedibra@yahoo.com>
2024-12-07 23:22:46 +0200

    Rewritten categories id to start with 1 instead of 0

--- src/schema/categories-tree.ts
+++ src/schema/categories-tree.ts
@@ -8,29 +8,27 @@ export type CategoryTreeItem = {
 
 export const categoriesTree: CategoryTreeItem[] = [
   {
-    id: 0,
+    id: 1,
     name_en: "Vehicles",
     name_ar: "عربيات",
     categories: [
       {
-        id: 1,
+        id: 2,
         name_en: "Electric Cars",
         name_ar: "عربيات كهرباء",
-        categories: [
-          { id: 2, name_en: "Tesla", name_ar: "تيسلا" },
-        ],
+        categories: [{ id: 3, name_en: "Tesla", name_ar: "تيسلا" }],
       },
-      { id: 3, name_en: "Gas Cars", name_ar: "عربيات غاز" },
+      { id: 4, name_en: "Gas Cars", name_ar: "عربيات غاز" },
     ],
   },
   {
-    id: 4,
+    id: 5,
     name_en: "Mobiles and Tablets",
     name_ar: "موبايلات و تابلت",
     categories: [
-      { id: 5, name_en: "Mobile Phones", name_ar: "موبايلات" },
-      { id: 6, name_en: "Tablets", name_ar: "تابلت" },
-      { id: 7, name_en: "Accessories", name_ar: "اكسسوارات" },
+      { id: 6, name_en: "Mobile Phones", name_ar: "موبايلات" },
+      { id: 7, name_en: "Tablets", name_ar: "تابلت" },
+      { id: 8, name_en: "Accessories", name_ar: "اكسسوارات" },
     ],
   },
 ];
@@ -39,6 +37,6 @@ export function toPathFormat(input: string): string {
   return input
     .trim()
     .toLowerCase()
-    .replace(/[^a-z0-9\s]/g, '')
-    .replace(/\s+/g, '-');
+    .replace(/[^a-z0-9\s]/g, "")
+    .replace(/\s+/g, "-");
 }
