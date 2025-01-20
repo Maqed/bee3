b110b489f93397207fe4ab5458e72f3e53a95e7b
Maqed <magedibra@yahoo.com>
2025-01-20 18:05:56 +0200

    feat: setup react query provider

--- src/providers/client-side.tsx
+++ src/providers/client-side.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "./theme-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useLocale } from "next-intl";
 import { DirectionProvider } from "@radix-ui/react-direction";
 import type { ReactNode } from "react";
@@ -9,14 +10,18 @@ type Props = {
   children: ReactNode;
 };
 
+const queryClient = new QueryClient();
+
 function ClientSideProviders({ children }: Props) {
   const locale = useLocale();
   return (
     <SessionProvider>
       <ThemeProvider attribute="class" defaultTheme="light">
-        <DirectionProvider dir={locale === "ar" ? "rtl" : "ltr"}>
-          {children}
-        </DirectionProvider>
+        <QueryClientProvider client={queryClient}>
+          <DirectionProvider dir={locale === "ar" ? "rtl" : "ltr"}>
+            {children}
+          </DirectionProvider>
+        </QueryClientProvider>
       </ThemeProvider>
     </SessionProvider>
   );
