f0d4428bb90543fb02c41f66638df69c6524c197
Maqed <magedibra@yahoo.com>
2025-01-21 02:59:31 +0200

    feat: add PROTECTED_ROUTES and ONLY_UNAUTHENTICATED_ROUTES

--- src/consts/routes.ts
+++ src/consts/routes.ts
@@ -1,3 +1,9 @@
 export const DEFAULT_LOGIN_REDIRECT = "/";
 
 export const DEFAULT_UNAUTHENTICATED_REDIRECT = "/login";
+
+// Protected routes that require authentication
+export const PROTECTED_ROUTES = ["/favorites", "/sell", "/user-settings"];
+
+// Authentication routes that signed-in users shouldn't access
+export const ONLY_UNAUTHENTICATED_ROUTES = ["/login", "/register"];
