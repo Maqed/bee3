2cb147b7bb59877d88a981a586448166fe45cb9a
Maqed <magedibra@yahoo.com>
2025-01-21 16:53:28 +0200

    feat: include seller information in the ad

--- src/app/api/bee3/ad/[adId]/route.ts
+++ src/app/api/bee3/ad/[adId]/route.ts
@@ -1,16 +1,28 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 import { db } from "@/server/db";
 
-
-export async function GET(request: Request, context: { params: { adId: string } }) {
+export async function GET(
+  request: Request,
+  context: { params: { adId: string } },
+) {
   const { params } = context;
 
   try {
-    const ad = await db.ad.findUniqueOrThrow({ where: { id: params.adId } });
+    const ad = await db.ad.findUniqueOrThrow({
+      where: { id: params.adId },
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+            createdAt: true,
+          },
+        },
+      },
+    });
 
     return NextResponse.json({ ad });
-  }
-  catch (e) {
+  } catch (e) {
     return NextResponse.json({ error: e }, { status: 404 });
   }
 }
