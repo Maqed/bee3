ca33fd826a0008bca5939b3b51b0dd156a433f89
Maqed <magedibra@yahoo.com>
2025-01-23 20:22:06 +0200

    feat: add Suspense for AdsCategoriesCarousels

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,13 +1,22 @@
+import { Suspense } from "react";
 import AdsCategoriesCarousels from "./_components/ads-categories-carousels";
 import ExploreCategories from "./_components/explore-categories";
-
 import SellButton from "@/components/bee3/sell-button";
+import AdsCarouselPlaceholder from "@/components/placeholders/ads-carousel-placeholder";
 
 export default async function HomePage() {
   return (
     <main className="flex flex-col gap-y-5">
       <ExploreCategories />
-      <AdsCategoriesCarousels />
+      {/* TODO: Change the Array length to the actual number of carousel in prod  */}
+      <Suspense
+        fallback={[...Array(2)].map(() => (
+          <AdsCarouselPlaceholder />
+        ))}
+      >
+        <AdsCategoriesCarousels />
+      </Suspense>
+
       <SellButton className="fixed bottom-10 left-1/2 -translate-x-1/2 transform md:hidden" />
     </main>
   );
