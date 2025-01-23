b620213298d165d2740937eb786822d2585548d7
Maqed <magedibra@yahoo.com>
2025-01-23 20:22:20 +0200

    feat: add AdsCarouselPlaceholder

---
+++ src/components/placeholders/ads-carousel-placeholder.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import AdCardPlaceholder from "./ad-card-placeholder";
+import { Skeleton } from "@/components/ui/skeleton";
+
+function AdsCarouselPlaceholder() {
+  return (
+    <section className="container w-full">
+      <div className="mb-3 flex items-center justify-between">
+        <Skeleton className="h-7 w-64" />
+        <Skeleton className="h-5 w-24" />
+      </div>
+      <Carousel opts={{ dragFree: true }} className="w-full max-w-full">
+        <CarouselContent>
+          {[...Array(4)].map(() => (
+            <CarouselItem>
+              <AdCardPlaceholder orientation="vertical" />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </section>
+  );
+}
+
+export default AdsCarouselPlaceholder;
