9ae57d8dee0e244079ea9371564e8f96702291fc
Maqed <magedibra@yahoo.com>
2025-01-24 01:14:19 +0200

    feat: add useFavAds hook to get ads

---
+++ src/hooks/useFavAds.tsx
@@ -0,0 +1,22 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { type Ad } from "@prisma/client";
+
+interface FavoriteAdsResponse {
+  favoriteAds: Ad[];
+}
+
+export function useFavoriteAds() {
+  return useQuery<FavoriteAdsResponse, Error>({
+    queryKey: ["favorite-ads"],
+    queryFn: async () => {
+      const response = await fetch("/api/bee3/favAds");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch favorite ads");
+      }
+
+      return response.json();
+    },
+  });
+}
