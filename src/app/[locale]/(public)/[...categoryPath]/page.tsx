a387b05a4d0da0477a1c77508f83ffc76d809c6c
Maqed <magedibra@yahoo.com>
2025-01-24 17:52:13 +0200

    feat: add metadata

--- src/app/[locale]/(public)/[...categoryPath]/page.tsx
+++ src/app/[locale]/(public)/[...categoryPath]/page.tsx
@@ -1,9 +1,32 @@
 import ShowingAdsPage from "@/components/bee3/showing-ads/showing-ads-page";
+import {
+  getServerSideCategory,
+  getServerSideSubCategory,
+} from "@/lib/server-side";
+import { getCategoryAndSubCategory } from "@/lib/utils";
+import { getTranslations } from "next-intl/server";
 
 type Props = {
   params: { categoryPath: string[] };
   searchParams: { [key: string]: string | undefined };
 };
+
+export async function generateMetadata({ params }: Props) {
+  const { category, subCategory } = getCategoryAndSubCategory(
+    params.categoryPath,
+  );
+
+  const localizedCategory = subCategory
+    ? await getServerSideSubCategory(category, subCategory)
+    : await getServerSideCategory(category);
+
+  const t = await getTranslations("/[categoryPath].metadata");
+
+  return {
+    title: localizedCategory,
+    description: t("description", { category: localizedCategory }),
+  };
+}
 
 function CategoryPathPage({ params, searchParams }: Props) {
   return (
