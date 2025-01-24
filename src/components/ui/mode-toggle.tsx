7bd470743c4446a2fca7ca35b89857c8abd868fe
Maqed <magedibra@yahoo.com>
2025-01-24 16:46:08 +0200

    feat: reverse display for both sun and moon and fill them with colors

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -16,8 +16,8 @@ export default function ModeToggle() {
       size="icon"
       onClick={() => setTheme(theme === "light" ? "dark" : "light")}
     >
-      <Sun className="h-[1.5rem] w-[1.3rem] dark:hidden" />
-      <Moon className="hidden h-5 w-5 dark:block" />
+      <Sun className="hidden h-[1.5rem] w-[1.3rem] fill-yellow-500 text-yellow-500 dark:block" />
+      <Moon className="h-5 w-5 fill-foreground dark:hidden" />
       <span className="sr-only">{t("toggle-theme")}</span>
     </Button>
   );
