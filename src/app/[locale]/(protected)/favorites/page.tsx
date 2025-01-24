d215e0f8eb113e372bb4db0b799d4408f2af6504
Maqed <magedibra@yahoo.com>
2025-01-24 02:48:50 +0200

    feat: add heart icon

--- src/app/[locale]/(protected)/favorites/page.tsx
+++ src/app/[locale]/(protected)/favorites/page.tsx
@@ -1,6 +1,7 @@
 import { getTranslations } from "next-intl/server";
 import Navbar from "@/components/navbar/navbar";
 import { FavoriteAds } from "./_components/favorite-ads";
+import { Heart } from "lucide-react";
 
 async function FavoritesPage() {
   const tFavorites = await getTranslations("/favorites");
@@ -8,7 +9,10 @@ async function FavoritesPage() {
     <div>
       <Navbar />
       <main className="container mt-4 sm:mx-auto md:max-w-5xl">
-        <h1 className="text-3xl font-black">{tFavorites("title")}</h1>
+        <h1 className="mb-5 flex items-center gap-3 text-3xl font-black">
+          <Heart className="inline size-8 fill-red-600 text-red-600" />{" "}
+          {tFavorites("title")}
+        </h1>
         <FavoriteAds />
       </main>
     </div>
