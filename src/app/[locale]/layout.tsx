3a3c8587fd0f180170434328d1248ead84ee6ee0
Maqed <magedibra@yahoo.com>
2025-01-24 18:20:05 +0200

    feat: internationalize metadata

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,47 +1,55 @@
 import "@/styles/globals.css";
 import { GeistSans } from "geist/font/sans";
 import { Toaster } from "@/components/ui/toaster";
-import { type Metadata } from "next";
-import { env } from "@/env";
 import ServerSideProviders from "@/providers/server-side";
 import ClientSideProviders from "@/providers/client-side";
+import { getMessages, getTranslations } from "next-intl/server";
 
-const title = "Bee3";
-const description =
-  "Bee3 is a platform made for selling and buying online completely for free!";
-export const metadata: Metadata = {
-  metadataBase: new URL(env.NEXTAUTH_URL),
-  title: {
-    default: title,
-    template: `%s | ${title}`,
-  },
-  keywords: ["Bee3", "Sell", "Buy", "Shop", "Live"],
-  description,
-  openGraph: {
-    title: title,
-    description: description,
-    url: env.NEXTAUTH_URL,
-    siteName: title,
-    locale: "en_US",
-    type: "website",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: title,
-    description: description,
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}) {
+  const t = await getTranslations("metadata");
+  const messages = await getMessages();
+  const title = t("title");
+  const description = t("description");
+  // @ts-ignore
+  const keywords = Object.values(messages.metadata.keywords);
+
+  return {
+    title: {
+      default: title,
+      template: `%s | ${title}`,
+    },
+    description,
+    keywords,
+    openGraph: {
+      title,
+      description,
+      siteName: title,
+      url: process.env.NEXTAUTH_URL,
+      locale: locale === "ar" ? "ar_EG" : "en_US",
+      type: "website",
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+    },
+    robots: {
       index: true,
       follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
     },
-  },
-};
+  };
+}
 
 export default function RootLayout({
   children,
