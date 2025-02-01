7d5d3223783d3f662711a1961b0bdf7d2a7f67ae
Maqed <magedibra@yahoo.com>
2025-02-01 02:44:21 +0200

    feat: clean code and add contact-info

--- src/app/[locale]/(public)/ad/[adId]/_components/ad-page-placeholder.tsx
+++ src/app/[locale]/(public)/ad/[adId]/_components/ad-page-placeholder.tsx
@@ -1,4 +1,4 @@
-import type React from "react";
+"use client";
 import { Skeleton } from "@/components/ui/skeleton";
 import {
   Card,
@@ -8,81 +8,160 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { MapPin, MessageCircle, Phone } from "lucide-react";
+import { MapPin, Phone } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
-import AdsCarouselPlaceholder from "@/components/placeholders/ads-carousel-placeholder";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import Whatsapp from "@/components/icons/whatsapp";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from "@/components/ui/carousel";
+import AdCardPlaceholder from "@/components/placeholders/ad-card-placeholder";
 
-function AdPagePlaceholder() {
+export default function AdPagePlaceholder() {
   return (
     <>
-      {/* Ad Information */}
-      <div className="col-span-12 flex flex-col gap-y-5 md:col-span-8">
-        <Skeleton className="h-[450px] w-full" />
-        <div className="flex flex-col gap-y-5 max-md:container">
-          {/* Price and Title */}
-          <Card>
-            <CardHeader>
-              <CardTitle className="flex items-center justify-between">
-                <Skeleton className="h-10 w-40" />
-                <Skeleton className="h-6 w-6" />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Skeleton className="mb-2 h-8 w-3/4" />
-              <div className="flex items-center justify-between">
-                <div className="flex w-1/3 items-center">
-                  <MapPin className="me-2 inline-flex animate-pulse text-muted" />
-                  <Skeleton className="h-5 w-full" />
-                </div>
-                <Skeleton className="h-5 w-1/4" />
-              </div>
-            </CardContent>
-          </Card>
-          {/* Description */}
-          <Card>
-            <CardHeader>
-              <CardTitle>
-                <Skeleton className="h-6 w-1/4" />
-              </CardTitle>
-            </CardHeader>
-            <CardContent>
-              <Skeleton className="h-20 w-full" />
-            </CardContent>
-          </Card>
+      <div className="col-span-12 flex flex-col gap-y-5 pb-20 md:col-span-8 md:pb-0">
+        {renderAdImagesPlaceholder()}
+        <div className="flex flex-col gap-y-5 max-sm:mx-1">
+          {renderPriceAndTitlePlaceholder()}
+          {renderDescriptionPlaceholder()}
+          {renderUserInformationMobilePlaceholder()}
           <Separator />
-          {/* Related Ads */}
-          <AdsCarouselPlaceholder />
+          {renderRelatedAdsPlaceholder()}
         </div>
+        {renderContactInfoMobilePlaceholder()}
       </div>
-      {/* User Information */}
-      <div className="hidden md:col-span-4 md:block">
-        <Card>
-          <CardHeader>
-            <CardTitle>
-              <Skeleton className="h-6 w-1/2" />
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Skeleton className="mb-2 h-6 w-3/4" />
-            <Skeleton className="h-4 w-1/2" />
-          </CardContent>
-          <CardFooter className="flex flex-col gap-y-3 *:w-full">
-            <Button disabled>
-              <Phone className="me-1" /> <Skeleton className="h-4 w-24" />
-            </Button>
-            <Button disabled>
-              <MessageCircle className="me-1" />{" "}
-              <Skeleton className="h-4 w-24" />
-            </Button>
-            <Button disabled variant="whatsapp">
-              <MessageCircle className="me-1" />{" "}
-              <Skeleton className="h-4 w-24" />
-            </Button>
-          </CardFooter>
-        </Card>
-      </div>
+      {renderUserInformationDesktopPlaceholder()}
     </>
   );
 }
 
-export default AdPagePlaceholder;
+function renderAdImagesPlaceholder() {
+  return (
+    <div className="space-y-3">
+      <Skeleton className="h-[450px] w-full" />
+      <ScrollArea className="w-full whitespace-nowrap rounded-md border">
+        <div className="flex w-max gap-x-2 p-1 pb-3">
+          {[...Array(5)].map((_, index) => (
+            <Skeleton key={index} className="h-[100px] w-[100px]" />
+          ))}
+        </div>
+        <ScrollBar orientation="horizontal" />
+      </ScrollArea>
+    </div>
+  );
+}
+
+function renderPriceAndTitlePlaceholder() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="flex items-center justify-between">
+          <Skeleton className="h-10 w-40" />
+          <Skeleton className="h-6 w-6" />
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="mb-2 h-8 w-3/4" />
+        <div className="flex items-center justify-between">
+          <div className="flex w-1/3 items-center">
+            <MapPin className="me-2 inline-flex animate-pulse text-muted-foreground" />
+            <Skeleton className="h-5 w-full" />
+          </div>
+          <Skeleton className="h-5 w-1/4" />
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+function renderDescriptionPlaceholder() {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>
+          <Skeleton className="h-6 w-1/4" />
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="h-20 w-full" />
+      </CardContent>
+    </Card>
+  );
+}
+
+function renderUserInformationMobilePlaceholder() {
+  return (
+    <Card className="md:hidden">
+      <CardHeader>
+        <CardTitle>
+          <Skeleton className="h-6 w-1/2" />
+        </CardTitle>
+      </CardHeader>
+      <CardContent>
+        <Skeleton className="mb-2 h-6 w-3/4" />
+        <Skeleton className="h-4 w-1/2" />
+      </CardContent>
+    </Card>
+  );
+}
+
+function renderRelatedAdsPlaceholder() {
+  return (
+    <div className="space-y-3">
+      <Skeleton className="h-6 w-1/4" />
+      <Carousel className="w-full max-w-full">
+        <CarouselContent>
+          {[...Array(4)].map((_, index) => (
+            <CarouselItem key={`ad-card-placeholder-${index}`}>
+              <AdCardPlaceholder />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+}
+
+function renderContactInfoMobilePlaceholder() {
+  return (
+    <div className="fixed bottom-0 start-0 flex w-full items-center justify-center gap-3 border-t bg-background py-3 md:hidden">
+      <Button disabled size="lg">
+        <Phone className="me-2" />
+      </Button>
+      <Button disabled size="lg" variant="whatsapp">
+        <Whatsapp className="me-2 size-6" />
+      </Button>
+    </div>
+  );
+}
+
+function renderUserInformationDesktopPlaceholder() {
+  return (
+    <div className="hidden md:col-span-4 md:block">
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <Skeleton className="h-6 w-1/2" />
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Skeleton className="mb-2 h-6 w-3/4" />
+          <Skeleton className="h-4 w-1/2" />
+        </CardContent>
+        <CardFooter className="flex flex-col gap-y-3">
+          <Button disabled className="w-full">
+            <Phone className="me-2" />
+            <Skeleton className="h-4 w-24" />
+          </Button>
+          <Button disabled variant="whatsapp" className="w-full">
+            <Whatsapp className="me-2 size-6" />
+            <Skeleton className="h-4 w-24" />
+          </Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
