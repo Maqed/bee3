46200cec13d01c229db0776bd0ff5f30fdca0fa7
Maqed <magedibra@yahoo.com>
2025-02-02 01:53:11 +0200

    feat: add tokenStorage when user signs up

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,8 +1,10 @@
 import { betterAuth } from "better-auth";
+import { createAuthMiddleware } from "better-auth/api";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { db } from "@/server/db";
 import { env } from "@/env";
 import { headers } from "next/headers";
+import { AdTiers } from "@prisma/client";
 
 export const auth = betterAuth({
   database: prismaAdapter(db, {
@@ -20,6 +22,51 @@ export const auth = betterAuth({
       clientId: env.FACEBOOK_CLIENT_ID,
       clientSecret: env.FACEBOOK_CLIENT_SECRET,
     },
+  },
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path.startsWith("/sign-up")) {
+        const newSession = ctx.context.newSession;
+        if (!newSession) return;
+        const getRefreshTime = (days: number) => {
+          const date = new Date(Date.now());
+          date.setDate(date.getDate() + days);
+          return date;
+        };
+        await db.user.update({
+          where: { id: newSession.user.id },
+          data: {
+            tokensStorage: {
+              createMany: {
+                data: [
+                  {
+                    tokenType: AdTiers.Free,
+                    initialCount: 20,
+                    count: 20,
+                    refreshInDays: 7,
+                    nextRefreshTime: getRefreshTime(7),
+                  },
+                  {
+                    tokenType: AdTiers.Pro,
+                    initialCount: 3,
+                    count: 3,
+                    refreshInDays: 0,
+                    nextRefreshTime: new Date(0),
+                  },
+                  {
+                    tokenType: AdTiers.Expert,
+                    initialCount: 1,
+                    count: 1,
+                    refreshInDays: 0,
+                    nextRefreshTime: new Date(0),
+                  },
+                ],
+              },
+            },
+          },
+        });
+      }
+    }),
   },
   user: {
     additionalFields: {
