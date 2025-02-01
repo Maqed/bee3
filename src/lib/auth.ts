98773395b621fa885a0b2bb132fd6617030b0609
Maqed <magedibra@yahoo.com>
2025-02-02 00:21:54 +0200

    chore: use db instance instead of create new instance

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,10 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
-import { PrismaClient } from "@prisma/client";
+import { db } from "@/server/db";
 import { env } from "@/env";
 
-const prisma = new PrismaClient();
 export const auth = betterAuth({
-  database: prismaAdapter(prisma, {
+  database: prismaAdapter(db, {
     provider: "postgresql",
   }),
   emailAndPassword: {
