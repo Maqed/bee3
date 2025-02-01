4fd4faeb62636c046a2fae67e4fd195c343b2750
Maqed <magedibra@yahoo.com>
2025-02-02 01:59:31 +0200

    chore: phone-input related code

--- src/components/auth/user-phone-button/phone-input-otp.tsx
+++ src/components/auth/user-phone-button/phone-input-otp.tsx
@@ -1,137 +1,138 @@
-"use client";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { checkPhoneNumberOTP } from "@/schema/twilio";
-import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormMessage,
-  FormField,
-  FormItem,
-} from "@/components/ui/form";
-import {
-  InputOTP,
-  InputOTPGroup,
-  InputOTPSeparator,
-  InputOTPSlot,
-} from "@/components/ui/input-otp";
-import { dialogStates } from "./user-phone-button";
-import { InputProps } from "@/components/ui/input";
-import { cn } from "@/lib/utils";
-import { useTransition } from "react";
-import { useLocale, useTranslations } from "next-intl";
-import { AlertDialogFooter } from "@/components/ui/alert-dialog";
-import { useSession } from "next-auth/react";
+// TODO: FIX: MAKE IT WORK
+// "use client";
+// import { zodResolver } from "@hookform/resolvers/zod";
+// import { useForm } from "react-hook-form";
+// import { z } from "zod";
+// import { checkPhoneNumberOTP } from "@/schema/twilio";
+// import { Button } from "@/components/ui/button";
+// import {
+//   Form,
+//   FormControl,
+//   FormDescription,
+//   FormMessage,
+//   FormField,
+//   FormItem,
+// } from "@/components/ui/form";
+// import {
+//   InputOTP,
+//   InputOTPGroup,
+//   InputOTPSeparator,
+//   InputOTPSlot,
+// } from "@/components/ui/input-otp";
+// import { dialogStates } from "./user-phone-button";
+// import { InputProps } from "@/components/ui/input";
+// import { cn } from "@/lib/utils";
+// import { useTransition } from "react";
+// import { useLocale, useTranslations } from "next-intl";
+// import { AlertDialogFooter } from "@/components/ui/alert-dialog";
+// import { useSession } from "next-auth/react";
 
-const MAX_LENGTH = 6;
+// const MAX_LENGTH = 6;
 
-type Props = {
-  phoneNumber: string;
-  setIsDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  setDialogState: React.Dispatch<React.SetStateAction<dialogStates>>;
-} & InputProps;
+// type Props = {
+//   phoneNumber: string;
+//   setIsDialogOpen: React.Dispatch<React.SetStateAction<boolean>>;
+//   setDialogState: React.Dispatch<React.SetStateAction<dialogStates>>;
+// } & InputProps;
 
-function PhoneInputOTP({
-  className,
-  phoneNumber,
-  setDialogState,
-  setIsDialogOpen,
-}: Props) {
-  const { update: updateSessionUI } = useSession();
-  const [isPending, startTransition] = useTransition();
-  const tPhoneNumber = useTranslations("phoneNumber.OTP");
-  const locale = useLocale();
+// function PhoneInputOTP({
+//   className,
+//   phoneNumber,
+//   setDialogState,
+//   setIsDialogOpen,
+// }: Props) {
+//   const { update: updateSessionUI } = useSession();
+//   const [isPending, startTransition] = useTransition();
+//   const tPhoneNumber = useTranslations("phoneNumber.OTP");
+//   const locale = useLocale();
 
-  const form = useForm<z.infer<typeof checkPhoneNumberOTP>>({
-    resolver: zodResolver(checkPhoneNumberOTP),
-    defaultValues: {
-      code: "",
-      phoneNumber,
-    },
-  });
-  function onSubmit(values: z.infer<typeof checkPhoneNumberOTP>) {
-    startTransition(async () => {
-      try {
-        const response = await fetch("/api/twilio/verify-otp", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify(values),
-        });
+//   const form = useForm<z.infer<typeof checkPhoneNumberOTP>>({
+//     resolver: zodResolver(checkPhoneNumberOTP),
+//     defaultValues: {
+//       code: "",
+//       phoneNumber,
+//     },
+//   });
+//   function onSubmit(values: z.infer<typeof checkPhoneNumberOTP>) {
+//     startTransition(async () => {
+//       try {
+//         const response = await fetch("/api/twilio/verify-otp", {
+//           method: "POST",
+//           headers: { "Content-Type": "application/json" },
+//           body: JSON.stringify(values),
+//         });
 
-        const result = await response.json();
-        if (result.error) {
-          form.setError("code", {
-            message: tPhoneNumber(`errors.${result.error}`),
-          });
-        } else {
-          setIsDialogOpen(false);
-          await updateSessionUI();
-        }
-      } catch (err) {
-        form.setError("code", {
-          message: tPhoneNumber("errors.failed-verify-phone"),
-        });
-      }
-    });
-  }
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className={cn("flex flex-col gap-3", className)}
-      >
-        <FormField
-          control={form.control}
-          disabled={isPending}
-          name="code"
-          render={({ field }) => (
-            <FormItem>
-              <div
-                className={cn({
-                  "flex justify-end": locale === "ar",
-                })}
-                dir="ltr"
-              >
-                <FormControl>
-                  <InputOTP maxLength={MAX_LENGTH} {...field}>
-                    <InputOTPGroup>
-                      <InputOTPSlot index={0} />
-                      <InputOTPSlot index={1} />
-                      <InputOTPSlot index={2} />
-                    </InputOTPGroup>
-                    <InputOTPSeparator />
-                    <InputOTPGroup>
-                      <InputOTPSlot index={3} />
-                      <InputOTPSlot index={4} />
-                      <InputOTPSlot index={5} />
-                    </InputOTPGroup>
-                  </InputOTP>
-                </FormControl>
-              </div>
-              <FormDescription>
-                {tPhoneNumber("otp-description")}
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+//         const result = await response.json();
+//         if (result.error) {
+//           form.setError("code", {
+//             message: tPhoneNumber(`errors.${result.error}`),
+//           });
+//         } else {
+//           setIsDialogOpen(false);
+//           await updateSessionUI();
+//         }
+//       } catch (err) {
+//         form.setError("code", {
+//           message: tPhoneNumber("errors.failed-verify-phone"),
+//         });
+//       }
+//     });
+//   }
+//   return (
+//     <Form {...form}>
+//       <form
+//         onSubmit={form.handleSubmit(onSubmit)}
+//         className={cn("flex flex-col gap-3", className)}
+//       >
+//         <FormField
+//           control={form.control}
+//           disabled={isPending}
+//           name="code"
+//           render={({ field }) => (
+//             <FormItem>
+//               <div
+//                 className={cn({
+//                   "flex justify-end": locale === "ar",
+//                 })}
+//                 dir="ltr"
+//               >
+//                 <FormControl>
+//                   <InputOTP maxLength={MAX_LENGTH} {...field}>
+//                     <InputOTPGroup>
+//                       <InputOTPSlot index={0} />
+//                       <InputOTPSlot index={1} />
+//                       <InputOTPSlot index={2} />
+//                     </InputOTPGroup>
+//                     <InputOTPSeparator />
+//                     <InputOTPGroup>
+//                       <InputOTPSlot index={3} />
+//                       <InputOTPSlot index={4} />
+//                       <InputOTPSlot index={5} />
+//                     </InputOTPGroup>
+//                   </InputOTP>
+//                 </FormControl>
+//               </div>
+//               <FormDescription>
+//                 {tPhoneNumber("otp-description")}
+//               </FormDescription>
+//               <FormMessage />
+//             </FormItem>
+//           )}
+//         />
 
-        <AlertDialogFooter className="flex sm:space-x-0">
-          <Button
-            variant="link"
-            type="button"
-            onClick={form.handleSubmit(onSubmit)}
-            disabled={isPending}
-          >
-            {tPhoneNumber("verifyCode")}
-          </Button>
-        </AlertDialogFooter>
-      </form>
-    </Form>
-  );
-}
+//         <AlertDialogFooter className="flex sm:space-x-0">
+//           <Button
+//             variant="link"
+//             type="button"
+//             onClick={form.handleSubmit(onSubmit)}
+//             disabled={isPending}
+//           >
+//             {tPhoneNumber("verifyCode")}
+//           </Button>
+//         </AlertDialogFooter>
+//       </form>
+//     </Form>
+//   );
+// }
 
-export default PhoneInputOTP;
+// export default PhoneInputOTP;
