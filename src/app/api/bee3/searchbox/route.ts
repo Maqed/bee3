5b01c6af97d259af6ed9d638d2066ced177aa5cb
Maqed <magedibra@yahoo.com>
2025-02-02 07:17:17 +0200

    feat: change response format

--- src/app/api/bee3/searchbox/route.ts
+++ src/app/api/bee3/searchbox/route.ts
@@ -9,7 +9,11 @@ export async function GET(request: NextRequest) {
   if (!search)
     return NextResponse.json({ error: "invalid-query" }, { status: 400 });
 
-  const searchQuery = search.trim().split(" ").map(word => `${word}:*`).join(" & ");
+  const searchQuery = search
+    .trim()
+    .split(" ")
+    .map((word) => `${word}:*`)
+    .join(" & ");
 
   const ads = await db.ad.findMany({
     orderBy: [
@@ -24,35 +28,56 @@ export async function GET(request: NextRequest) {
     where: {
       title: { search: searchQuery },
     },
+    include: {
+      category: {
+        select: {
+          name_ar: true,
+          name_en: true,
+        },
+      },
+    },
     take: 8,
   });
 
-  const categorizedHits: { title: string; category: string }[] = [];
+  const categorizedHits: {
+    title: string;
+    category: {
+      categoryPath: string;
+      name_ar: string;
+      name_en: string;
+    };
+  }[] = [];
   const uncategorizedHits: string[] = [];
   const usedCategories = new Set<string>();
 
-  const isQueryStrong = ads.length > 0 && search.trim().length / ads[0]!.title.trim().split(' ')[0]!.length >= 0.8;
+  const isQueryStrong =
+    ads.length > 0 &&
+    search.trim().length / ads[0]!.title.trim().split(" ")[0]!.length >= 0.8;
 
   for (const ad of ads) {
-    const categoryName = ad.categoryPath;
-    if (!categoryName || !isQueryStrong) {
+    const { categoryPath, category } = ad;
+    if (!categoryPath || !isQueryStrong) {
       uncategorizedHits.push(ad.title);
       continue;
     }
 
-    if (!usedCategories.has(categoryName)) {
+    if (!usedCategories.has(categoryPath)) {
       categorizedHits.push({
         title: ad.title,
-        category: categoryName,
+        category: {
+          categoryPath,
+          name_en: category?.name_en!,
+          name_ar: category?.name_ar!,
+        },
       });
-      usedCategories.add(categoryName);
+      usedCategories.add(categoryPath);
     } else {
       uncategorizedHits.push(ad.title);
     }
   }
 
   return NextResponse.json({
-    "categorizedHits": categorizedHits,
-    "uncategorizedHits": uncategorizedHits,
+    categorizedHits,
+    uncategorizedHits,
   });
 }
