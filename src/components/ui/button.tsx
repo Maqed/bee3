48a1b319ccd0a9273890a9b658670f982d85fc6b
Maqed <magedibra@yahoo.com>
2025-02-10 08:17:11 +0200

    feat: change whatsapp colors to brand colors

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,7 +16,7 @@ const buttonVariants = cva(
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        whatsapp: "bg-green-400",
+        whatsapp: "bg-[#25d366] dark:bg-[#2A8E80]",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
