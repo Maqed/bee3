325315d21b05c7569f650ad7d20e6cac8ce6f53d
Maqed <magedibra@yahoo.com>
2025-02-23 18:35:34 +0200

    fix: images url not being sent

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -16,7 +16,13 @@ export async function GET(request: NextRequest) {
       select: {
         name: true,
         bio: true,
-        ads: true,
+        ads: {
+          include: {
+            images: {
+              select: { url: true },
+            },
+          },
+        },
       },
     });
     console.log({ user });
