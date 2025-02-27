c35f3431aaa9d46b02299958281ca37ee5367b13
Maqed <magedibra@yahoo.com>
2025-02-27 10:07:51 +0200

    feat: add forgot password page

---
+++ src/app/[locale]/(auth)/(must-be-logged-out)/forgot-password/page.tsx
@@ -0,0 +1,8 @@
+import ForgetPasswordForm from "@/components/auth/forgot-password-form";
+import React from "react";
+
+function ForgotPasswordPage() {
+  return <ForgetPasswordForm />;
+}
+
+export default ForgotPasswordPage;
