2d0f23dc8cc63620a04784e18cdb879769341e37
Maqed <magedibra@yahoo.com>
2025-02-27 10:38:03 +0200

    feat: add Reset password

---
+++ src/app/[locale]/(auth)/(must-be-logged-out)/reset-password/page.tsx
@@ -0,0 +1,8 @@
+import ResetPasswordForm from "@/components/auth/reset-password-form";
+import React from "react";
+
+function ResetPasswordPage() {
+  return <ResetPasswordForm />;
+}
+
+export default ResetPasswordPage;
