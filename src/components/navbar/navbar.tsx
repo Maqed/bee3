6240dc84023557cd9d31b03dbec1b58d6f22a7e5
Maqed <magedibra@yahoo.com>
2025-03-05 13:30:04 +0200

    feat: remove sheet component as it was used only for internationalization

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,17 +1,7 @@
-"use client";
 import { Link } from "@/navigation";
-import { Menu } from "lucide-react";
 import NavbarAuth from "./navbar-auth";
 import LocaleSwitcher from "@/components/ui/locale-switcher";
-import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
 import ModeToggle from "@/components/ui/mode-toggle";
-import {
-  Sheet,
-  SheetContent,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
-} from "@/components/ui/sheet";
 import SellButton from "../bee3/sell-button";
 import AdSearch from "../bee3/search/ad-searchbox";
 import Logo from "../bee3/logo";
@@ -26,28 +16,10 @@ export default function Navbar() {
           </h1>
         </Link>
         <nav className="flex items-center gap-x-2">
-          {/* Shown in desktop, Hidden in mobile */}
-          <div className="flex items-center gap-x-2 max-md:hidden">
-            <LocaleSwitcher />
-          </div>
+          <LocaleSwitcher />
           <SellButton className="hidden md:flex" />
           <ModeToggle />
           <NavbarAuth />
-          {/* Mobile Nav */}
-          <Sheet>
-            <SheetTrigger className="md:hidden">
-              <Menu className="h-5 w-5" />
-            </SheetTrigger>
-            <SheetContent className="pt-10">
-              <VisuallyHidden>
-                <SheetHeader>
-                  <SheetTitle>Nav Links</SheetTitle>
-                </SheetHeader>
-              </VisuallyHidden>
-              {/* Shown in mobile, Hidden in desktop */}
-              <LocaleSwitcher />
-            </SheetContent>
-          </Sheet>
         </nav>
       </div>
       <div className="flex items-center justify-center">
