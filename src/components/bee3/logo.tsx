2d4eff7ede8a8789c5844d6ad1914b7106a5c91a
Maqed <magedibra@yahoo.com>
2025-03-29 18:31:32 +0200

    feat: make the logo in english only

--- src/components/bee3/logo.tsx
+++ src/components/bee3/logo.tsx
@@ -5,9 +5,12 @@ import Image from "next/image";
 export function Logo() {
   const t = useTranslations();
   return (
-    <div className="flex flex-row items-center justify-center gap-1 text-lg font-bold text-primary">
+    <div
+      dir="ltr"
+      className="flex flex-row items-center justify-center gap-1 text-lg font-bold text-primary"
+    >
       <Image src="/bee3-logo.png" alt="logo" width={30} height={25} />{" "}
-      <span>{t("logo")}</span>
+      <span>Bee3</span>
     </div>
   );
 }
