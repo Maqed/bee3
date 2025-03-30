6301dc8971fe62dbfa1e452789381f815864f8d5
Maqed <magedibra@yahoo.com>
2025-03-30 23:16:19 +0200

    alternate between logos in light and dark mode

--- src/components/bee3/logo.tsx
+++ src/components/bee3/logo.tsx
@@ -1,12 +1,22 @@
+"use client";
 import Image from "next/image";
+import { useTheme } from "next-themes";
+import darkLogo from "/public/bee3-logo-dark.svg";
+import lightLogo from "/public/bee3-logo-light.svg";
 
 export function Logo() {
+  const { theme } = useTheme();
   return (
     <div
       dir="ltr"
       className="flex flex-row items-center justify-center gap-2 text-lg font-bold text-primary"
     >
-      <Image src="/bee3-logo.svg" alt="logo" width={40} height={40} />{" "}
+      <Image
+        src={theme === "dark" ? darkLogo : lightLogo}
+        alt="logo"
+        width={40}
+        height={40}
+      />
       <span>Bee3</span>
     </div>
   );
