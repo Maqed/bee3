ee26c1d76fb6210ea5f9533d743fc154a68e28f9
Maqed <magedibra@yahoo.com>
2025-04-01 00:25:45 +0200

    feat: improve the design of the sheet

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -14,10 +14,15 @@ import {
   SheetTrigger,
 } from "../ui/sheet";
 import { VisuallyHidden } from "@radix-ui/react-visually-hidden";
-import { Menu } from "lucide-react";
+import { Avatar } from "@/components/ui/avatar";
+import { ChevronsUpDown, Menu } from "lucide-react";
+import { DropdownMenuTrigger } from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { getLocale } from "next-intl/server";
 
 export default async function Navbar() {
   const session = await getServerAuthSession();
+  const locale = await getLocale();
   return (
     <header className="container sticky top-0 z-50 mb-5 flex flex-col justify-between gap-1 border-b bg-background py-4">
       <div className="flex items-center justify-between">
@@ -30,7 +35,14 @@ export default async function Navbar() {
             <LocaleSwitcher />
             <SellButton className="hidden md:flex" />
             <ModeToggle />
-            <NavbarAuth session={session} />
+            <NavbarAuth
+              trigger={
+                <DropdownMenuTrigger className="rounded-full focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2">
+                  <Avatar>{session?.user.name[0]}</Avatar>
+                </DropdownMenuTrigger>
+              }
+              session={session}
+            />
           </div>
           <Sheet>
             <SheetTrigger className="md:hidden">
@@ -42,11 +54,34 @@ export default async function Navbar() {
                   <SheetTitle>Navbar content</SheetTitle>
                 </SheetHeader>
               </VisuallyHidden>
-              <div className="flex h-full flex-col items-center justify-end gap-3 py-10">
-                <ModeToggle />
-                <SellButton />
-                <LocaleSwitcher />
-                <NavbarAuth session={session} />
+              <div className="flex h-full flex-col items-start justify-end gap-3">
+                <ModeToggle showToggleThemeText={true} className="w-full" />
+                <SellButton className="w-full" />
+                <LocaleSwitcher className="w-full" />
+                <NavbarAuth
+                  trigger={
+                    <DropdownMenuTrigger asChild>
+                      <Button
+                        variant="ghost"
+                        className="flex w-full items-center justify-between"
+                      >
+                        <ChevronsUpDown className="me-auto size-4" />
+                        <span className="truncate font-semibold">
+                          {session?.user.name}
+                        </span>
+                        <Avatar className="ms-2 h-8 w-8 rounded-lg">
+                          {session?.user.name[0]}
+                        </Avatar>
+                      </Button>
+                    </DropdownMenuTrigger>
+                  }
+                  dropdownMenuContentProps={{
+                    align: "start",
+                    side: locale == "en" ? "left" : "right",
+                    sideOffset: 6,
+                  }}
+                  session={session}
+                />
               </div>
             </SheetContent>
           </Sheet>
