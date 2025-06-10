1ca0a2bc3662f6f9dc28dba8d9227f2f6a26a23c
Maqed <magedibra@yahoo.com>
2025-06-10 03:22:41 +0300

    fix: upsert cities so they don't return an error if they already exist

--- prisma/seed/locationsSeeder.ts
+++ prisma/seed/locationsSeeder.ts
@@ -1,24 +1,26 @@
-import { cities } from '@/schema/cities';
-import { governorates } from '@/schema/governorates';
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
+import { cities } from "@/schema/cities";
+import { governorates } from "@/schema/governorates";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 export async function seedLocations() {
-    for (const governorate of governorates) {
-        await prisma.governorate.upsert({
-            where: { id: governorate.id },
-            update: {},
-            create: {
-                ...governorate
-            }
-        });
-    }
+  for (const governorate of governorates) {
+    await prisma.governorate.upsert({
+      where: { id: governorate.id },
+      update: {},
+      create: {
+        ...governorate,
+      },
+    });
+  }
 
-    for (const city of cities) {
-        await prisma.city.create({
-            data: {
-                ...city
-            }
-        });
-    }
+  for (const city of cities) {
+    await prisma.city.upsert({
+      where: { id: city.id },
+      update: {},
+      create: {
+        ...city,
+      },
+    });
+  }
 }
