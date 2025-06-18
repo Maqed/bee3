4693f8f859296ebf351a6451fc0a0bd9f395354d
Maqed <magedibra@yahoo.com>
2025-06-18 09:35:36 +0300

    fix: shadows

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,38 @@ const config = {
           foreground: "hsl(var(--card-foreground))",
         },
       },
+      boxShadow: {
+        sm: "0 1px 2px 0 hsl(var(--shadow) / 0.05)",
+        DEFAULT:
+          "0 1px 3px 0 hsl(var(--shadow) / 0.1), 0 1px 2px -1px hsl(var(--shadow) / 0.1)",
+        md: "0 4px 6px -1px hsl(var(--shadow) / 0.1), 0 2px 4px -2px hsl(var(--shadow) / 0.1)",
+        lg: "0 10px 15px -3px hsl(var(--shadow) / 0.1), 0 4px 6px -4px hsl(var(--shadow) / 0.1)",
+        xl: "0 20px 25px -5px hsl(var(--shadow) / 0.1), 0 8px 10px -6px hsl(var(--shadow) / 0.1)",
+        "2xl": "0 25px 50px -12px hsl(var(--shadow) / 0.25)",
+        inner: "inset 0 2px 4px 0 hsl(var(--shadow) / 0.05)",
+        none: "none",
+      },
+      dropShadow: {
+        sm: "0 1px 1px hsl(var(--shadow) / 0.05)",
+        DEFAULT: [
+          "0 1px 2px hsl(var(--shadow) / 0.1)",
+          "0 1px 1px hsl(var(--shadow) / 0.06)",
+        ],
+        md: [
+          "0 4px 3px hsl(var(--shadow) / 0.07)",
+          "0 2px 2px hsl(var(--shadow) / 0.06)",
+        ],
+        lg: [
+          "0 10px 8px hsl(var(--shadow) / 0.04)",
+          "0 4px 3px hsl(var(--shadow) / 0.1)",
+        ],
+        xl: [
+          "0 20px 13px hsl(var(--shadow) / 0.03)",
+          "0 8px 5px hsl(var(--shadow) / 0.08)",
+        ],
+        "2xl": "0 25px 25px hsl(var(--shadow) / 0.15)",
+        none: "0 0 #0000",
+      },
       borderRadius: {
         lg: "var(--radius)",
         md: "calc(var(--radius) - 2px)",
