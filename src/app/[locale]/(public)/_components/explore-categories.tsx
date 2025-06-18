2c3a233617cda10e0e025e53628c2262779d19ba
Maqed <magedibra@yahoo.com>
2025-06-18 19:23:12 +0300

    fix: mobile design

--- src/app/[locale]/(public)/_components/explore-categories.tsx
+++ src/app/[locale]/(public)/_components/explore-categories.tsx
@@ -48,7 +48,7 @@ function ExploreCategories() {
                     </Avatar>
                     <p className="text-sm">{categoryName}</p>
                   </DialogTrigger>
-                  <DialogContent className="max-w-2xl pb-0">
+                  <DialogContent className="max-w-2xl pb-0 max-sm:h-full">
                     <DialogHeader className="h-min">
                       <DialogTitle>
                         <Link tabIndex={-1} href={`/${categoryNamePathFormat}`}>
@@ -56,7 +56,7 @@ function ExploreCategories() {
                         </Link>
                       </DialogTitle>
                     </DialogHeader>
-                    <div className="flex flex-col items-start justify-start max-sm:max-h-96 max-sm:overflow-auto">
+                    <div className="flex flex-col items-start justify-start max-sm:max-h-[calc(100vh-100px)] max-sm:overflow-auto">
                       <Link
                         className="w-full text-primary hover:underline max-sm:text-center"
                         href={`/${categoryNamePathFormat}`}
@@ -66,7 +66,7 @@ function ExploreCategories() {
                       <div className="flex w-full items-center justify-center">
                         <Separator className="my-1 w-1/2" />
                       </div>
-                      <div className="grid h-full w-full grid-cols-3 gap-4 py-4">
+                      <div className="grid h-full w-full grid-cols-1 gap-4 py-4 max-sm:justify-items-start sm:grid-cols-3">
                         {category.categories?.map(async (subCategory) => {
                           const subCategoryNamePathFormat = toPathFormat(
                             subCategory.name,
@@ -84,7 +84,7 @@ function ExploreCategories() {
                             <Link
                               key={`explore-subcategory-${subCategoryNamePathFormat}`}
                               href={`/${categoryNamePathFormat}/${subCategoryNamePathFormat}`}
-                              className="group flex flex-col items-center justify-center"
+                              className="group flex items-center justify-center gap-1 sm:flex-col"
                             >
                               <Avatar className="rounded-md bg-primary/60">
                                 <SubCategoryIcon className="size-[18px]" />
