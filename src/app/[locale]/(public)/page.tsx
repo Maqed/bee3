aacd5151b23688f783f31dbc1670879579cb1a91
Maqed <magedibra@yahoo.com>
2025-06-19 16:28:56 +0300

    fix: revert

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,10 +1,23 @@
+import { Suspense } from "react";
 import AdsCategoriesCarousels from "./_components/ads-categories-carousels";
-import HomePageLayout from "@/components/bee3/home-page-layout";
+import ExploreCategories from "@/components/bee3/explore-categories";
+import SellButton from "@/components/bee3/sell-button";
+import AdsCarouselPlaceholder from "@/components/placeholders/ads-carousel-placeholder";
+import { categoriesTree } from "@/schema/categories-tree";
 
 export default function HomePage() {
   return (
-    <HomePageLayout>
-      <AdsCategoriesCarousels />
-    </HomePageLayout>
+    <main className="flex flex-col gap-y-5">
+      <ExploreCategories />
+      <Suspense
+        fallback={[...Array(categoriesTree.length)].map(() => (
+          <AdsCarouselPlaceholder />
+        ))}
+      >
+        <AdsCategoriesCarousels />
+      </Suspense>
+
+      <SellButton className="fixed bottom-10 left-1/2 -translate-x-1/2 transform md:hidden" />
+    </main>
   );
 }
