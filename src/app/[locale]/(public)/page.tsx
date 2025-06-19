863e52b8e7a6fb40b348ce093d26e02475991410
Maqed <magedibra@yahoo.com>
2025-06-19 16:22:23 +0300

    feat: optimize UX by adding loading.tsx

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -1,23 +1,10 @@
-import { Suspense } from "react";
 import AdsCategoriesCarousels from "./_components/ads-categories-carousels";
-import ExploreCategories from "@/components/bee3/explore-categories";
-import SellButton from "@/components/bee3/sell-button";
-import AdsCarouselPlaceholder from "@/components/placeholders/ads-carousel-placeholder";
-import { categoriesTree } from "@/schema/categories-tree";
+import HomePageLayout from "@/components/bee3/home-page-layout";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col gap-y-5">
-      <ExploreCategories />
-      <Suspense
-        fallback={[...Array(categoriesTree.length)].map(() => (
-          <AdsCarouselPlaceholder />
-        ))}
-      >
-        <AdsCategoriesCarousels />
-      </Suspense>
-
-      <SellButton className="fixed bottom-10 left-1/2 -translate-x-1/2 transform md:hidden" />
-    </main>
+    <HomePageLayout>
+      <AdsCategoriesCarousels />
+    </HomePageLayout>
   );
 }
---
+++ src/components/bee3/home-page-layout.tsx
@@ -0,0 +1,15 @@
+import { ReactNode } from "react";
+import ExploreCategories from "./explore-categories";
+import SellButton from "@/components/bee3/sell-button";
+
+function HomePageLayout({ children }: { children: ReactNode }) {
+  return (
+    <main className="flex flex-col gap-y-5">
+      <ExploreCategories />
+      {children}
+      <SellButton className="fixed bottom-10 left-1/2 -translate-x-1/2 transform md:hidden" />
+    </main>
+  );
+}
+
+export default HomePageLayout;
