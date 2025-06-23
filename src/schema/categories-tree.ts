3c1053b886630f7918a78cf1244cf51c436792a8
Maqed <magedibra@yahoo.com>
2025-06-23 12:46:30 +0300

    chore: lint

--- src/schema/categories-tree.ts
+++ src/schema/categories-tree.ts
@@ -31,10 +31,22 @@ export const categoriesTree: CategoryTreeItem[] = [
         name: "cars-for-sale",
         attributes: [
           { name: "horse-power", type: "number", unit: "HP" },
-          { name: "engine-type", type: "select", options: ["gasoline", "diesel", "hybrid", "electric"] },
+          {
+            name: "engine-type",
+            type: "select",
+            options: ["gasoline", "diesel", "hybrid", "electric"],
+          },
           { name: "engine-capacity", type: "number", unit: "CC" },
-          { name: "fuel-type", type: "select", options: ["petrol-benzine", "diesel", "hybrid", "electric"] },
-          { name: "transmission", type: "select", options: ["manual", "automatic"] },
+          {
+            name: "fuel-type",
+            type: "select",
+            options: ["petrol-benzine", "diesel", "hybrid", "electric"],
+          },
+          {
+            name: "transmission",
+            type: "select",
+            options: ["manual", "automatic"],
+          },
           { name: "seats", type: "number" },
           { name: "color", type: "text" },
           { name: "installation-location", type: "text" },
@@ -46,10 +58,22 @@ export const categoriesTree: CategoryTreeItem[] = [
         name: "cars-for-rent",
         attributes: [
           { name: "horse-power", type: "number", unit: "HP" },
-          { name: "engine-type", type: "select", options: ["gasoline", "diesel", "hybrid", "electric"] },
+          {
+            name: "engine-type",
+            type: "select",
+            options: ["gasoline", "diesel", "hybrid", "electric"],
+          },
           { name: "engine-capacity", type: "number", unit: "CC" },
-          { name: "fuel-type", type: "select", options: ["petrol-benzine", "diesel", "hybrid", "electric"] },
-          { name: "transmission", type: "select", options: ["manual", "automatic"] },
+          {
+            name: "fuel-type",
+            type: "select",
+            options: ["petrol-benzine", "diesel", "hybrid", "electric"],
+          },
+          {
+            name: "transmission",
+            type: "select",
+            options: ["manual", "automatic"],
+          },
           { name: "seats", type: "number" },
           { name: "color", type: "text" },
           { name: "installation-location", type: "text" },
@@ -61,15 +85,27 @@ export const categoriesTree: CategoryTreeItem[] = [
         name: "tyres-batteries-oils-accessories",
         inheritParentAttributes: false,
         attributes: [
-          { name: "category", type: "select", options: ["tyre", "battery", "oil", "accessory"] },
+          {
+            name: "category",
+            type: "select",
+            options: ["tyre", "battery", "oil", "accessory"],
+          },
           { name: "compatible-with", type: "text" },
           { name: "brand", type: "text" },
         ],
       },
       { id: 5, name: "car-spare-parts", inheritParentAttributes: false },
-      { id: 6, name: "motorcycles-accessories", inheritParentAttributes: false },
+      {
+        id: 6,
+        name: "motorcycles-accessories",
+        inheritParentAttributes: false,
+      },
       { id: 7, name: "boats-watercraft", inheritParentAttributes: false },
-      { id: 8, name: "heavy-trucks-buses-other-vehicles", inheritParentAttributes: false },
+      {
+        id: 8,
+        name: "heavy-trucks-buses-other-vehicles",
+        inheritParentAttributes: false,
+      },
     ],
   },
   {
@@ -100,9 +136,17 @@ export const categoriesTree: CategoryTreeItem[] = [
     attributes: [
       { name: "processor", type: "text" },
       { name: "gpu", type: "text" },
-      { name: "ram", type: "select", options: ["1GB", "2GB", "3GB", "4GB", "6GB", "8GB", "12GB", "16GB"] },
-      { name: "storage", type: "select", options: ["16GB", "32GB", "64GB", "128GB", "256GB", "512GB", "1tb"] },
-      { name: "screen-size", type: "number", unit: "Inches" }
+      {
+        name: "ram",
+        type: "select",
+        options: ["1GB", "2GB", "3GB", "4GB", "6GB", "8GB", "12GB", "16GB"],
+      },
+      {
+        name: "storage",
+        type: "select",
+        options: ["16GB", "32GB", "64GB", "128GB", "256GB", "512GB", "1tb"],
+      },
+      { name: "screen-size", type: "number", unit: "Inches" },
     ],
     categories: [
       { id: 20, name: "mobile-phones" },
@@ -113,20 +157,32 @@ export const categoriesTree: CategoryTreeItem[] = [
         inheritParentAttributes: false,
         attributes: [
           { name: "compatible-with", type: "text" },
-          { name: "type", type: "select", options: ["case", "screen-protector", "charger", "cable", "other"] }
-        ]
+          {
+            name: "type",
+            type: "select",
+            options: ["case", "screen-protector", "charger", "cable", "other"],
+          },
+        ],
       },
-      { id: 23, name: "mobile-numbers" }
+      { id: 23, name: "mobile-numbers" },
     ],
   },
   {
     id: 24,
     name: "jobs",
     attributes: [
-      { name: "employment-type", type: "select", options: ["full-time", "part-time", "contract"] },
+      {
+        name: "employment-type",
+        type: "select",
+        options: ["full-time", "part-time", "contract"],
+      },
       { name: "salary", type: "number" },
       { name: "experience", type: "number" },
-      { name: "education-level", type: "select", options: ["high-school", "bachelors", "masters", "phd"] }
+      {
+        name: "education-level",
+        type: "select",
+        options: ["high-school", "bachelors", "masters", "phd"],
+      },
     ],
     categories: [
       { id: 25, name: "accounting-finance-banking" },
@@ -146,7 +202,7 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 39, name: "secretarial" },
       { id: 40, name: "guards-security" },
       { id: 41, name: "legal-lawyers" },
-      { id: 42, name: "other-jobs" }
+      { id: 42, name: "other-jobs" },
     ],
   },
   {
@@ -161,7 +217,7 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 45, name: "computers-accessories" },
       { id: 46, name: "video-games-consoles" },
       { id: 47, name: "cameras-imaging" },
-      { id: 48, name: "home-appliances" }
+      { id: 48, name: "home-appliances" },
     ],
   },
   {
@@ -178,7 +234,7 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 54, name: "fabric-bedding-curtains" },
       { id: 55, name: "garden-outdoor" },
       { id: 56, name: "kitchenware" },
-      { id: 57, name: "lighting" }
+      { id: 57, name: "lighting" },
     ],
   },
   {
@@ -186,16 +242,20 @@ export const categoriesTree: CategoryTreeItem[] = [
     name: "fashion-beauty",
     attributes: [
       { name: "brand", type: "text" },
-      { name: "size", type: "select", options: ["XS", "S", "M", "L", "XL", "XXL", "special-sizes"] },
+      {
+        name: "size",
+        type: "select",
+        options: ["XS", "S", "M", "L", "XL", "XXL", "special-sizes"],
+      },
       { name: "condition", type: "select", options: ["new", "used"] },
-      { name: "color", type: "text" }
+      { name: "color", type: "text" },
     ],
     categories: [
       { id: 59, name: "womens-clothing" },
       { id: 60, name: "mens-clothing" },
       { id: 61, name: "womens-cosmetics" },
       { id: 62, name: "womens-accessories-personal-care" },
-      { id: 63, name: "mens-accessories-personal-care" }
+      { id: 63, name: "mens-accessories-personal-care" },
     ],
   },
   {
@@ -206,15 +266,19 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 66, name: "cats" },
       { id: 67, name: "birds-pigeons" },
       { id: 68, name: "other-pets-animals" },
-      { id: 69, name: "pet-accessories-care-products" }
+      { id: 69, name: "pet-accessories-care-products" },
     ],
   },
   {
     id: 70,
     name: "kids-babies",
     attributes: [
-      { name: "age-group", type: "select", options: ["newborn", "infant", "toddler", "preschooler", "school-age"] },
-      { name: "condition", type: "select", options: ["new", "used"] }
+      {
+        name: "age-group",
+        type: "select",
+        options: ["newborn", "infant", "toddler", "preschooler", "school-age"],
+      },
+      { name: "condition", type: "select", options: ["new", "used"] },
     ],
     categories: [
       { id: 71, name: "baby-mom-healthcare" },
@@ -222,14 +286,14 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 73, name: "baby-feeding-tools" },
       { id: 74, name: "cribs-strollers-carriers" },
       { id: 75, name: "toys" },
-      { id: 76, name: "other-baby-items" }
+      { id: 76, name: "other-baby-items" },
     ],
   },
   {
     id: 77,
     name: "hobbies-books-sports",
     attributes: [
-      { name: "condition", type: "select", options: ["new", "used"] }
+      { name: "condition", type: "select", options: ["new", "used"] },
     ],
     categories: [
       { id: 78, name: "antiques-collectibles" },
@@ -242,7 +306,7 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 85, name: "study-tools" },
       { id: 86, name: "tickets-vouchers" },
       { id: 87, name: "luggage" },
-      { id: 88, name: "other-items" }
+      { id: 88, name: "other-items" },
     ],
   },
   {
@@ -250,7 +314,7 @@ export const categoriesTree: CategoryTreeItem[] = [
     name: "business-industrial",
     attributes: [
       { name: "condition", type: "select", options: ["new", "used"] },
-      { name: "brand", type: "text" }
+      { name: "brand", type: "text" },
     ],
     categories: [
       { id: 90, name: "agriculture" },
@@ -260,7 +324,7 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 94, name: "office-furniture-equipment" },
       { id: 95, name: "restaurants-equipment" },
       { id: 96, name: "whole-business-for-sale" },
-      { id: 97, name: "other-business-industrial-agriculture" }
+      { id: 97, name: "other-business-industrial-agriculture" },
     ],
   },
   {
@@ -276,7 +340,7 @@ export const categoriesTree: CategoryTreeItem[] = [
       { id: 105, name: "movers" },
       { id: 106, name: "pet-services" },
       { id: 107, name: "education-services" },
-      { id: 108, name: "other-services" }
+      { id: 108, name: "other-services" },
     ],
-  }
+  },
 ];
