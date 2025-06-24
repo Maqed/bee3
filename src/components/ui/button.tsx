cb269284536d97688bcab4ce2e5d3136487fe692
Maqed <magedibra@yahoo.com>
2025-06-24 10:15:42 +0300

    feat: improve styles

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -18,14 +18,14 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         whatsapp: "bg-[#25d366] dark:bg-[#2A8E80]",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+        ghost: "hover:bg-primary/20 hover:text-primary-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
         default: "h-10 px-4 py-2",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-10 w-10 rounded-full",
       },
     },
     defaultVariants: {
