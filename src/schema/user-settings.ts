d5e7cd686757449c50c109520dcdbdffb134353f
Maqed <magedibra@yahoo.com>
2025-06-29 12:59:14 +0300

    fix: translation key

--- src/schema/user-settings.ts
+++ src/schema/user-settings.ts
@@ -6,5 +6,8 @@ export const userSettingsSchema = z.object({
     .min(2, { message: "/user-settings.name.invalid" })
     .max(20, { message: "/user-settings.name.invalid" }),
   bio: z.string().max(75, { message: "/user-settings.bio.invalid" }).optional(),
-  contactInfo: z.string().max(75, { message: "/user-settings.contactInfo.invalid" }).optional(),
+  contactInfo: z
+    .string()
+    .max(75, { message: "/user-settings.contact-info.invalid" })
+    .optional(),
 });
