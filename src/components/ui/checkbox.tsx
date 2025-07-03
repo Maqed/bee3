d5676456539cd3a3e8c8c2f0aa2fcbf5c11dfe8d
Maqed <magedibra@yahoo.com>
2025-07-03 15:59:36 +0300

    feat: add animations

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -1,30 +1,81 @@
 "use client";
+import * as RadixCheckbox from "@radix-ui/react-checkbox";
 
-import * as React from "react";
-import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { Check } from "lucide-react";
+import { AnimatePresence, motion } from "motion/react";
 
 import { cn } from "@/lib/utils";
 
-const Checkbox = React.forwardRef<
-  React.ElementRef<typeof CheckboxPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
->(({ className, ...props }, ref) => (
-  <CheckboxPrimitive.Root
-    ref={ref}
-    className={cn(
-      "peer size-6 shrink-0 rounded-sm border border-primary ring-offset-background transition-all duration-75 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
-      className,
-    )}
-    {...props}
-  >
-    <CheckboxPrimitive.Indicator
-      className={cn("flex items-center justify-center text-current")}
-    >
-      <Check className="size-5" />
-    </CheckboxPrimitive.Indicator>
-  </CheckboxPrimitive.Root>
-));
-Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+type CheckboxProps = React.CustomComponentPropsWithRef<
+  typeof RadixCheckbox.Root
+>;
 
-export { Checkbox };
+export function Checkbox({ checked, className, ...props }: CheckboxProps) {
+  return (
+    <RadixCheckbox.Root
+      {...props}
+      className={cn(
+        "relative inline-block size-6 appearance-none rounded border border-primary bg-muted",
+        className,
+      )}
+    >
+      <AnimatePresence mode="popLayout">
+        <RadixCheckbox.Indicator
+          className="absolute inset-0 flex items-center justify-center rounded-[inherit] bg-primary"
+          asChild
+        >
+          <motion.div
+            initial={{
+              opacity: 0,
+              scale: 0.5,
+            }}
+            animate={{
+              opacity: 1,
+              scale: 1,
+            }}
+            transition={{
+              duration: 0.3,
+              ease: [0.175, 0.885, 0.32, 1.1],
+            }}
+          >
+            <CheckIcon checkedState={checked} />
+          </motion.div>
+        </RadixCheckbox.Indicator>
+      </AnimatePresence>
+    </RadixCheckbox.Root>
+  );
+}
+
+type CheckIconProps = {
+  checkedState: CheckboxProps["checked"];
+};
+
+function CheckIcon({ checkedState }: CheckIconProps) {
+  const CHECK_PATH = "M5 13 L10 18 L20 6";
+  const INDETERMINATE_PATH = "M6 12 H18";
+
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="none"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="scale-65 shrink-0 stroke-primary-foreground stroke-2"
+    >
+      <title>Check</title>
+
+      <motion.path
+        d={checkedState === "indeterminate" ? INDETERMINATE_PATH : CHECK_PATH}
+        initial={{
+          pathLength: 0,
+        }}
+        animate={{
+          pathLength: 1,
+        }}
+        transition={{
+          duration: 0.3,
+          ease: [0.645, 0.045, 0.355, 1],
+        }}
+      />
+    </svg>
+  );
+}
