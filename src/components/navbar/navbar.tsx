29bd41ed634077fe6c4127c75e1295cd82a47a59
Maqed <magedibra@yahoo.com>
2025-07-03 16:38:50 +0300

    fix: navbar width styles

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -23,8 +23,8 @@ import { authClient } from "@/lib/auth-client";
 export default function Navbar() {
   const { data: session, isPending } = authClient.useSession();
   return (
-    <header className="container sticky top-0 z-50 mb-5 flex flex-col justify-between gap-1 border-b border-border/50 bg-background/80 py-4 shadow-sm backdrop-blur-md">
-      <div className="flex items-center justify-between">
+    <header className="sticky top-0 z-50 mb-5 flex flex-col justify-between gap-1 border-b border-border/50 bg-background/80 py-4 shadow-sm backdrop-blur-md">
+      <div className="container flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Logo />
         </Link>
