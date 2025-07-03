f8589c01406b1eb479dbf8a48dc8b34cd176841f
Maqed <magedibra@yahoo.com>
2025-07-03 16:19:27 +0300

    chore: add padding bottom

--- src/app/[locale]/(auth)/layout.tsx
+++ src/app/[locale]/(auth)/layout.tsx
@@ -7,7 +7,7 @@ export default async function RootLayout({
   return (
     <>
       <Navbar />
-      <main className="h-screen-without-navbar flex w-full items-center justify-center">
+      <main className="h-screen-without-navbar flex w-full items-center justify-center pb-5">
         {children}
       </main>
     </>
