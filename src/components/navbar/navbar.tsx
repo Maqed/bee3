fa2de179726fb8bd0bdbc56fdca33db79bddfc47
Maqed <magedibra@yahoo.com>
2025-07-11 18:49:29 +0300

    feat: give the navbar the background of the cards

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -23,7 +23,7 @@ import { authClient } from "@/lib/auth-client";
 export default function Navbar() {
   const { data: session, isPending } = authClient.useSession();
   return (
-    <header className="sticky top-0 z-50 mb-5 flex flex-col justify-between gap-1 border-b border-border/50 bg-background/80 py-4 shadow-sm backdrop-blur-md">
+    <header className="sticky top-0 z-50 mb-5 flex flex-col justify-between gap-1 border-b border-border/50 bg-card/80 py-4 shadow-sm backdrop-blur-md">
       <div className="container flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2">
           <Logo />
