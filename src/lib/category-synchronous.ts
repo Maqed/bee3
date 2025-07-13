fa2ecf798c89966adaaaf3b5a554ddc5204a0993
Maqed <magedibra@yahoo.com>
2025-07-13 16:09:39 +0300

    fix: full category

--- src/lib/category-synchronous.ts
+++ src/lib/category-synchronous.ts
@@ -1,14 +1,47 @@
 import { useTranslations } from "next-intl";
-import { getCategoryAndSubCategory } from "./category";
 
 export function useCategoryTranslations() {
   const tCategory = useTranslations("categories");
 
-  const getSynchronousFullCategory = (categoryPath: string) => {
-    const { category, subCategory } = getCategoryAndSubCategory(categoryPath);
-    if (!subCategory) return getSynchronousCategory(category);
-    return `${getSynchronousCategory(category)} - ${getSynchronousSubCategory(category, subCategory)}`;
-  };
+  // Function overloads for type safety
+  function getSynchronousFullCategory(
+    pathSegments: string[] | string,
+    options: {
+      returnAs: "array";
+    },
+  ): string[];
+  function getSynchronousFullCategory(
+    pathSegments: string[] | string,
+    options?: {
+      returnAs: "string";
+    },
+  ): string;
+  function getSynchronousFullCategory(
+    pathSegments: string[] | string,
+    options?: {
+      returnAs: "string" | "array";
+    },
+  ): string | string[] {
+    // Convert string to array if necessary
+    const segments = Array.isArray(pathSegments)
+      ? pathSegments
+      : pathSegments.split("/");
+
+    if (segments.length === 0) return "";
+
+    let translationPath = segments[0];
+    let name = tCategory(`${segments[0]}.name`);
+
+    for (let i = 1; i < segments.length; i++) {
+      translationPath += `.categories.${segments[i]}`;
+      name += ` - ${tCategory(`${translationPath}.name`)}`;
+    }
+    if (options?.returnAs == "array") {
+      return name.split(" - ");
+    }
+
+    return name;
+  }
 
   const getSynchronousCategory = (category: string) => {
     return tCategory(`${category}.name`);
