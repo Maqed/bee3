4f55dfbf3f464a4b40bf2d5035f27317c0e51de4
Maqed <magedibra@yahoo.com>
2025-07-14 19:18:54 +0300

    feat: add admin default page

---
+++ src/app/[locale]/(protected)/admin/page.tsx
@@ -0,0 +1,12 @@
+import { getServerAuthSession } from "@/lib/auth";
+import { redirect } from "@/navigation";
+
+async function AdminRoute() {
+  const session = await getServerAuthSession();
+  if (!session || session.user.role !== "admin") {
+    redirect("/");
+  }
+  redirect("/admin/users");
+}
+
+export default AdminRoute;
