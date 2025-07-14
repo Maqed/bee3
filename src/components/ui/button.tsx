c24c773d084d0d204d7f8dff7aa3487602fb376f
Maqed <magedibra@yahoo.com>
2025-07-14 20:08:31 +0300

    feat: add button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -13,6 +13,9 @@ const buttonVariants = cva(
           "bg-primary/75 text-primary-foreground dark:text-white hover:bg-primary/90",
         destructive:
           "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        success: "bg-success text-success-foreground hover:bg-success/90",
+        warning: "bg-warning text-warning-foreground hover:bg-warning/90",
+        info: "bg-info text-info-foreground hover:bg-info/90",
         outline:
           "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
         secondary:
