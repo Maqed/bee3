3cdcd34f574ddc4a37cd82777ca36365bbfe054d
Maqed <magedibra@yahoo.com>
2025-07-14 14:55:40 +0300

    feat: get all ads except the deleted ones

--- src/app/api/bee3/ad/[adId]/route.ts
+++ src/app/api/bee3/ad/[adId]/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from "next/server";
 import { db } from "@/server/db";
+import { getServerAuthSession } from "@/lib/auth";
+import { getNonDeletedAdById } from "@/database/ad";
 
 export async function GET(
   request: Request,
@@ -8,8 +10,7 @@ export async function GET(
   const { params } = context;
 
   try {
-    let ad = await db.ad.findUniqueOrThrow({
-      where: { id: params.adId },
+    let ad = await getNonDeletedAdById(params.adId, {
       include: {
         user: {
           select: {
@@ -40,6 +41,27 @@ export async function GET(
     });
 
     return NextResponse.json({ ad });
+  } catch (e) {
+    return NextResponse.json({ error: e }, { status: 404 });
+  }
+}
+
+export async function DELETE(
+  request: Request,
+  context: { params: { adId: string } },
+) {
+  const { params } = context;
+  const session = await getServerAuthSession();
+  if (!session) return NextResponse.json({ error: "must-be-logged-in" });
+
+  try {
+    // When adding admin plugin, Make sure that the admin and the user can delete the ad.
+    const ad = await db.ad.update({
+      where: { id: params.adId, userId: session.user.id },
+      data: { deletedAt: new Date() },
+    });
+
+    return NextResponse.json({ message: "Ad deleted successfully", ad });
   } catch (e) {
     return NextResponse.json({ error: e }, { status: 404 });
   }
