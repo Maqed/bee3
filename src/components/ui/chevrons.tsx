0b7d6d128feb319e661aeb4ddd4b5160988fe0dd
Maqed <magedibra@yahoo.com>
2025-07-14 20:16:55 +0300

    feat: add chevrons

--- src/components/ui/chevrons.tsx
+++ src/components/ui/chevrons.tsx
@@ -1,6 +1,12 @@
 "use client";
 import { useLocale } from "next-intl";
-import { ChevronLeft, ChevronRight, type LucideProps } from "lucide-react";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ChevronsLeft,
+  ChevronsRight,
+  type LucideProps,
+} from "lucide-react";
 import { cn } from "@/lib/utils";
 
 export const ForwardChevron = ({ className, ...props }: LucideProps) => {
@@ -22,6 +28,36 @@ export const BackwardChevron = ({ className, ...props }: LucideProps) => {
   const locale = useLocale();
   return (
     <ChevronLeft
+      className={cn(
+        {
+          "rotate-180": locale === "ar",
+        },
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+export const ForwardChevrons = ({ className, ...props }: LucideProps) => {
+  const locale = useLocale();
+  return (
+    <ChevronsRight
+      className={cn(
+        {
+          "rotate-180": locale === "ar",
+        },
+        className,
+      )}
+      {...props}
+    />
+  );
+};
+
+export const BackwardChevrons = ({ className, ...props }: LucideProps) => {
+  const locale = useLocale();
+  return (
+    <ChevronsLeft
       className={cn(
         {
           "rotate-180": locale === "ar",
