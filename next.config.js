885b451f1f3757eccf570cee8e7323d07bd9fc85
Maqed <magedibra@yahoo.com>
2025-07-22 20:04:04 +0300

    chore: revert to how it was before

--- next.config.js
+++ next.config.js
@@ -2,19 +2,15 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-
 await import("./src/env.js");
+import createNextIntlPlugin from "next-intl/plugin";
 
-const createNextIntlPlugin = require("next-intl/plugin");
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import("next").NextConfig} */
 const config = {
   images: {
-    unoptimized: true,
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["@prisma/client", ".prisma/client"],
+    unoptimized: true
   },
 };
 
