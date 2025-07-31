784679f4fad30e8cc9e81a3a3cb1e9e95483c674
Maqed <magedibra@yahoo.com>
2025-07-31 22:51:52 +0300

    chore: change logo colors, text

--- src/components/bee3/logo.tsx
+++ src/components/bee3/logo.tsx
@@ -1,22 +1,13 @@
-"use client";
 import Image from "next/image";
-import { useTheme } from "next-themes";
-import darkLogo from "/public/bee3-logo-dark.svg";
-import lightLogo from "/public/bee3-logo-light.svg";
+import logo from "/public/logo.svg";
 
 export function Logo() {
-  const { theme } = useTheme();
   return (
     <div
       dir="ltr"
       className="flex flex-row items-center justify-center gap-2 text-lg font-bold text-primary"
     >
-      <Image
-        src={theme === "dark" ? darkLogo : lightLogo}
-        alt="logo"
-        width={40}
-        height={40}
-      />
+      <Image src={logo} alt="logo" width={40} height={40} />
       <span>Bee3</span>
     </div>
   );
