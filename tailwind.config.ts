46a80068ea732cea0ee96f9c6047868d116191ba
Maqed <magedibra@yahoo.com>
2025-08-08 04:54:40 +0300

    chore: change container padding styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,12 @@ const config = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "3rem",
+        lg: "4rem",
+      },
       screens: {
         "2xl": "1400px",
       },
