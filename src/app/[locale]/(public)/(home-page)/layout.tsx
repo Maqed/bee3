fc9f41a13c5f61f8acac47755e21ffbc5a012581
Maqed <magedibra@yahoo.com>
2025-08-20 14:20:00 +0300

    feat: add home page banner

--- src/app/[locale]/(public)/(home-page)/layout.tsx
+++ src/app/[locale]/(public)/(home-page)/layout.tsx
@@ -1,9 +1,23 @@
 import { ReactNode } from "react";
 import ExploreCategories from "@/components/bee3/explore-categories";
 import SellButton from "@/components/bee3/sell-button";
+import Image from "next/image";
+import { Link } from "@/i18n/navigation";
+import { useLocale } from "next-intl";
+
 function HomePageLayout({ children }: { children: ReactNode }) {
+  const locale = useLocale();
   return (
     <main className="flex flex-col gap-y-5">
+      <Link href="/about-us" className="container">
+        <Image
+          src={`/home-banner-${locale}.svg`}
+          alt=""
+          width={500}
+          height={100}
+          className="h-auto w-full"
+        />
+      </Link>
       <ExploreCategories />
       {children}
       <SellButton className="fixed bottom-4 left-1/2 -translate-x-1/2 transform bg-primary/95 md:hidden" />
